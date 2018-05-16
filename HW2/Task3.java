fe7f2fa32f1b5d91097c756390a70bdd58a05944
NickSkidan <mikolaskidan17@gmail.com>
2018-05-16 14:36:33 +0300

    Task3 was changed

--- HW2/Task3.java
+++ HW2/Task3.java
@@ -1,7 +1,7 @@
 public class Task3 {
     public static void main(String[] args) {
         boolean b;
-        int[] nums = {3, 15, 7, 65, 97, 132, 133, 14, 13, 16, 17, 94, 77, 81};
+        int[] nums = {-2, 15, 7, 65, 97, 132, 133, 1, 14, 13, 16, 17, 3, 94, 77, 81};
         for (int i : nums) {
             b = true;
             for (int j = 2; j < i / 2; j++) {
@@ -9,7 +9,7 @@ public class Task3 {
                     b = false;
                 }
             }
-            if (b) System.out.println(i + " простое");
+            if (b & (i > 1)) System.out.println(i + " простое");
         }
     }
 }

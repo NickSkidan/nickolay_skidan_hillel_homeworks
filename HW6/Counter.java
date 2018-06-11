9c7b4c36817209419ee3dc85cee827908dc8653d
NickSkidan <mikolaskidan17@gmail.com>
2018-06-11 16:14:33 +0300

    added not ready HW6 (Exception)

---
+++ HW6/Counter.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+public class Counter {
+    double num1, num2;
+    String str1, str2;
+
+    public void Scan() {
+        try {
+            str1 = JOptionPane.showInputDialog("Введите первое число");
+            str2 = JOptionPane.showInputDialog("Введите второе число");
+            num1 = Double.parseDouble(str1);
+            num2 = Double.parseDouble(str2);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Введите число");
+        }
+    }
+
+    public void Scan(double i, double k) {
+        JOptionPane.showMessageDialog(null, "Теперь первое число равно " + i);
+        JOptionPane.showMessageDialog(null, "Теперь второе число равно " + k);
+    }
+}
---
+++ HW6/TestCounter.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class TestCounter {
+    public static void main(String[] args) {
+        Counter counter = new Counter();
+        counter.Scan();
+        counter.num1 = count(counter.num1);
+        counter.num2 = count(counter.num2);
+        counter.Scan(counter.num1, counter.num2);
+    }
+
+    private static double count(double num) {
+        num = 100 / num;
+        return num;
+    }
+}
